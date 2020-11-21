a1719a901ce4488f6bd801e9ec9e13485ee350b1
Juan Camilo Morales <camov1209@gmail.com>
2020-11-21 17:42:20 -0500

    setup assitant conversation

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions');
+const { conversation } = require('@assistant/conversation');
 
+const app = conversation();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,3 +9,9 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+app.handle('create_user', conv => {
+  conv.add('Hi, how is it going?');
+});
+
+exports.fulfillment = functions.https.onRequest(app);
