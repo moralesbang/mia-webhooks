5e9e81abf7230d6ce5afd70cf2770fb584165e59
juangome <jupabass89@gmail.com>
2020-11-23 14:31:22 -0500

    adding some new flows to the userId validation

--- functions/index.js
+++ functions/index.js
@@ -8,80 +8,85 @@ const HANDLERS = {
   validateUserById: 'validate_id'
 }
 
-// Firebase
+// Utils
+const logJson = (value) =>
+  console.log('JSON LOGGED 👀 -->', JSON.stringify(value))
+
+// Firebase SetUp
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
 admin.initializeApp(functions.config().firebase)
 const db = admin.firestore()
 
-function getUserByEmail (email, name) {
-  // get user by email
-  const usersRef = db.collection('users')
-  const query = usersRef.where('email', '==', email).where('name', '==', name)
-  query.get().then(snapshot => {
-    if (snapshot.empty) {
-      logJson('No matching user')
-      return
-    }
-    return true
-  })
+// Firebase Functions
+async function getUserByEmail (email) {
+  return db
+    .collection('users')
+    .where('email', '==', email)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        logJson('No matching user')
+        return false
+      }
+      return true
+    })
     .catch(() => {
       logJson('Error getting user')
     })
 }
 
-function getUserById (id) {
-  // get user by email
-  const usersRef = db.collection('users')
-  const query = usersRef.where('id', '==', id)
-  query.get().then(snapshot => {
-    if (snapshot.empty) {
-      logJson('No matching user')
-      return
-    }
-    return true
-  })
+async function getUserById (id) {
+  return db
+    .collection('users')
+    .where('id', '==', id)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        logJson('No matching user')
+        return false
+      }
+      return true
+    })
     .catch(() => {
       logJson('Error getting user')
     })
 }
 
-// Utils
-const logJson = value => console.log('JSON LOGGED 👀 -->', JSON.stringify(value))
-
-// Setup
+// Setup conversation
 const app = conversation({
   clientId: functions.config().client.id
 })
 
 // Handlers
-app.handle(HANDLERS.createUser, conv => {
+app.handle(HANDLERS.createUser, (conv) => {
   logJson(conv)
   conv.add('Registro completado!')
 })
 
-app.handle(HANDLERS.validateUserByEmail, conv => {
-  logJson(conv)
-  const userName = conv.user.name
+app.handle(HANDLERS.validateUserByEmail, (conv) => {
   const email = conv.user.email
-  const response = getUserByEmail(email, userName)
+  const response = getUserByEmail(email)
+  logJson(response)
   if (!response) {
     // response the handler false
   }
   // response the handler ok
 })
 
-app.handle(HANDLERS.validateUserById, conv => {
+app.handle(HANDLERS.validateUserById, async (conv) => {
   logJson(conv)
-  const id = conv.user.id // validate
-  const response = getUserById(id)
+  const id = conv.scene.slots.userId.value // validate
+  const response = await getUserById(id)
+  logJson(response)
   if (!response) {
-    // response the handler false
+    conv.scene.next.name = 'UserOnBoarding'
+  } else {
+    conv.add('El usuario está registrado!')
   }
-  // response the handler ok
 })
 
-app.handle('greeting', conv => {
+app.handle('greeting', (conv) => {
   logJson(conv)
 })
 
