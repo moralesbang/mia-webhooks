054a5816b5fa2f8b92d63c0fb0413cd3fc2f0eb8
juangome <jupabass89@gmail.com>
2020-11-24 19:50:01 -0500

    getting the user data from the validate_email webhook

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,3 @@
-// const functions = require('firebase-functions')
-const { conversation } = require('@assistant/conversation')
-
 // Constants
 const HANDLERS = {
   createUser: 'create_user',
@@ -18,98 +15,90 @@ const functions = require('firebase-functions')
 admin.initializeApp(functions.config().firebase)
 const db = admin.firestore()
 
+// Setup conversation
+const { conversation } = require('@assistant/conversation')
+const app = conversation({
+  clientId: functions.config().client.id
+})
+
 // Firebase Functions
-async function getUserByEmail (email) {
-  return db
-    .collection('users')
-    .where('email', '==', email)
-    .get()
-    .then((snapshot) => {
-      if (snapshot.empty) {
-        logJson('No matching user')
-        return false
-      }
-      return true
-    })
-    .catch(() => {
-      logJson('Error getting user')
-    })
-}
-
-async function getUserById (id) {
-  return db
-    .collection('users')
-    .where('id', '==', id)
-    .get()
-    .then((snapshot) => {
-      if (snapshot.empty) {
-        logJson('No matching user')
-        return false
-      }
-      return true
-    })
-    .catch(() => {
-      logJson('Error getting user')
-    })
-}
-
-async function addUser (name) {
+async function addUser (userData) {
+  // consultar primero si ya existe
+  // .
+  // .
+  // .
+  // //
   const docRef = db.collection('users').doc()
   try {
-    docRef.set({
-      name: name,
-      id: '87654321',
-      email: 'mompi@getUserByEmail.com'
-    })
+    docRef.set({ ...userData })
     return true
-    // return Promise.all(setAda)
   } catch (error) {
     return false
   }
 }
 
-// Setup conversation
-const app = conversation({
-  clientId: functions.config().client.id
-})
+async function getUserByEmail (email) {
+  const query = await db.collection('users').where('email', '==', email).get()
+  try {
+    const snapshot = query.docs[0]
+    const data = snapshot.data()
+    return data
+  } catch (error) {
+    return null
+  }
+}
 
 // Handlers
-app.handle(HANDLERS.createUser, async (conv) => {
-  logJson(conv)
-  const name = conv.scene.slots.userName.value // validate
-  const response = await addUser(name)
-  logJson(response)
+app.handle(HANDLERS.validateUserByEmail, async (conv) => {
+  const email = conv.user.params.tokenPayload.email
+  const response = await getUserByEmail(email)
   if (response) {
-    // conv.scene.next.name = 'HelloWord'
-    conv.add('El usuario fue creado exitosamente!')
-  }
-})
-
-app.handle(HANDLERS.validateUserByEmail, (conv) => {
-  const email = conv.user.email
-  const response = getUserByEmail(email)
-  logJson(response)
-  if (!response) {
-    conv.scene.next.name = 'UserOnBoarding'
+    conv.add(response.plate)
   } else {
-    conv.add('El usuario ya está registrado!')
+    conv.add('Usuario no registrado')
   }
+  logJson(response)
 })
 
 app.handle(HANDLERS.validateUserById, async (conv) => {
   logJson(conv)
   const id = conv.scene.slots.userId.value // validate
-  const response = await getUserById(id)
-  logJson(response)
-  if (!response) {
-    conv.scene.next.name = 'UserOnBoarding'
-  } else {
-    conv.add('El usuario ya está registrado!')
-  }
+  logJson(id)
+  // const response = await getUserById(id)
+  // logJson(response)
+  // if (!response) {
+  //   // conv.scene.next.name = 'UserOnBoarding'
+  // } else {
+  //   conv.add('El usuario ya está registrado!')
+  // }
 })
 
-app.handle('greeting', (conv) => {
+app.handle(HANDLERS.createUser, async (conv) => {
   logJson(conv)
+  // original data
+  // const userData = {
+  //   name: conv.scene.slots.userName.value,
+  //   id: conv.scene.slots.userId.value,
+  //   email: conv.scene.slots.email.value,
+  //   insurance: conv.scene.slots.insurance.value,
+  //   plate: conv.scene.slots.plate.value,
+  // }
+  // mock data
+  const userData = {
+    name: 'Carlos Duque',
+    id: '1234567',
+    email: 'carlosduque@udea.edu.co',
+    insurance: 'sura',
+    plate: 'abc123'
+  }
+  const response = await addUser(userData)
+  if (response) {
+    conv.add('Usuario creado exitosamente!')
+    // validateInsurance(userData, conv)
+  } else {
+    conv.add('No fue posible crear el usuario')
+    conv.scene.next.name = 'ErrorScene'
+  }
 })
 
 exports.fulfillment = functions.https.onRequest(app)
