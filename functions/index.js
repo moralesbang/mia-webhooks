e216425293ebbb2dd63ad5395856fd919c936a06
juangome <jupabass89@gmail.com>
2020-11-25 10:20:46 -0500

    improving the addUsuer function

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,13 @@ const HANDLERS = {
 const logJson = (value) =>
   console.log('JSON LOGGED 👀 -->', JSON.stringify(value))
 
+// const isEmpty = async (object) => {
+//   if (!object || (Object.keys(object).length === 0 && object.constructor === Object)) {
+//     return true
+//   }
+//   return false
+// }
+
 // Firebase SetUp
 const admin = require('firebase-admin')
 const functions = require('firebase-functions')
@@ -23,17 +30,18 @@ const app = conversation({
 
 // Firebase Functions
 async function addUser (userData) {
-  // consultar primero si ya existe
-  // .
-  // .
-  // .
-  // //
-  const docRef = db.collection('users').doc()
-  try {
-    docRef.set({ ...userData })
-    return true
-  } catch (error) {
+  const emailExist = await getUserByEmail(userData.email)
+  const idExist = await getUserById(userData.id)
+  if (emailExist || idExist) {
     return false
+  } else {
+    const docRef = db.collection('users').doc()
+    try {
+      docRef.set({ ...userData })
+      return true
+    } catch (error) {
+      return false
+    }
   }
 }
 
@@ -59,49 +67,67 @@ async function getUserById (id) {
   }
 }
 
+// salesforce functions
+async function validateInsurance (userData, conv) {
+  logJson(conv)
+  // salesforce integration
+  //
+  const insurance = true
+  //
+  //
+
+  if (insurance) {
+    conv.scene.next.name = 'ServiceSelection'
+  } else {
+    conv.add('El usuario no tiene un seguro activo con: ' + userData.insurance + ' ')
+    conv.scene.next.name = 'EndScene'
+  }
+}
+
 // Handlers
 app.handle(HANDLERS.validateUserByEmail, async (conv) => {
   const email = conv.user.params.tokenPayload.email
   const response = await getUserByEmail(email)
   if (response) {
-    conv.add(response.plate)
+    conv.add(
+      'El usuario con email: ' + response.email + 'ya está registrado! '
+    )
+    validateInsurance(response, conv)
   } else {
     conv.add('Usuario no registrado ')
     conv.scene.next.name = 'CompleteProfile'
   }
-  logJson(response)
 })
 
 app.handle(HANDLERS.validateUserById, async (conv) => {
-  logJson(conv)
-  const id = conv.scene.slots.userId.value // validate
-  logJson(id)
+  const id = conv.session.params.id
   const response = await getUserById(id)
   if (response) {
-    conv.add(response.name)
+    conv.add('El usuario con ID: ' + response.id + 'ya está registrado! ')
+    validateInsurance(response, conv)
   } else {
-    conv.add('Usuario no registrado')
+    conv.add('Usuario no registrado ')
+    conv.scene.next.name = 'CompleteProfile'
   }
-  logJson(response)
 })
 
 app.handle(HANDLERS.createUser, async (conv) => {
-  logJson(conv)
-  // original data
+  const email = conv.user.params.tokenPayload.email ? conv.user.params.tokenPayload.email : conv.session.params.email
+  const name = conv.user.params.tokenPayload.name ? conv.user.params.tokenPayload.name : conv.session.params.name
   const userData = {
-    name: conv.user.params.tokenPayload.name,
+    name,
+    email,
     id: conv.session.params.id,
-    email: conv.user.params.tokenPayload.email,
     insurance: conv.session.params.plate,
     plate: conv.session.params.insurance
   }
   const response = await addUser(userData)
   if (response) {
-    conv.add('Usuario creado exitosamente!')
-    // validateInsurance(userData, conv)
+    conv.add('Usuario creado exitosamente! ')
+    validateInsurance(response, conv)
   } else {
-    conv.add('No fue posible crear el usuario')
-    // conv.scene.next.name = 'ErrorScene'
+    conv.add('No fue posible crear el usuario, ')
+    conv.scene.next.name = 'ErrorScene'
   }
 })
 
